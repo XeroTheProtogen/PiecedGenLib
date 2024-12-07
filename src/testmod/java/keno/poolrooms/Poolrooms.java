e4251de21f47f845cccb28ee632ce27578c23cc9
KenopsicManiac <db318330@gmail.com>
2024-12-08 08:17:26 +1300

    Setup test mod

---
+++ src/testmod/java/keno/poolrooms/Poolrooms.java
@@ -0,0 +1,10 @@
+package keno.poolrooms;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Poolrooms implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
